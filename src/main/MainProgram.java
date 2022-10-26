5f9be20bee32ef22e7c197b265e7b002d7349120
chavdar77 <chavmom@gmail.com>
2022-10-26 16:33:36 +0300

    Initial commit  Changes to be committed: 	new file:   .classpath 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   bin/lab/Lab.class 	new file:   bin/main/MainProgram.class 	new file:   bin/maindisplay/MainDisplayClass.class 	new file:   bin/menus/MenuDemoClass.class 	new file:   bin/menus/submenus/SubMenu1.class 	new file:   bin/menus/submenus/SubMenu2.class 	new file:   bin/models/MedicalTechnologist.class 	new file:   bin/models/Pacient.class 	new file:   bin/models/TypeOfResearch.class 	new file:   bin/service/Service.class 	new file:   bin/threads/FirstRunnableResearch.class 	new file:   bin/threads/SecondRunnableResearch.class 	new file:   src/lab/Lab.java 	new file:   src/main/MainProgram.java 	new file:   src/maindisplay/MainDisplayClass.java 	new file:   src/menus/MenuDemoClass.java 	new file:   src/menus/submenus/SubMenu1.java 	new file:   src/menus/submenus/SubMenu2.java 	new file:   src/models/MedicalTechnologist.java 	new file:   src/models/Pacient.java 	new file:   src/models/TypeOfResearch.java 	new file:   src/service/Service.java 	new file:   src/threads/FirstRunnableResearch.java 	new file:   src/threads/SecondRunnableResearch.java

---
+++ src/main/MainProgram.java
@@ -0,0 +1,39 @@
+package main;
+
+
+//import lab.Lab;
+import maindisplay.MainDisplayClass;
+//import service.Service;
+//import threads.FirstRunnableResearch;
+//import threads.SecondRunnableResearch;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+	
+//	Lab lab = new Lab();
+//	Service service = new Service();
+//	lab = service.getInfoLab();
+//	
+//	Runnable  firstRunnableThread = new FirstRunnableResearch(lab);
+//	Thread firstThread = new Thread(firstRunnableThread, "First Thread");
+//	
+//	Runnable secondRunnableThread = new SecondRunnableResearch(lab);
+//	Thread secondThread = new Thread(secondRunnableThread, "Second Thread");
+//	
+//	firstThread.start();
+//	try {
+//	    firstThread.join();
+//	} catch (InterruptedException e) {
+//	    e.printStackTrace();
+//	}
+//	secondThread.start();
+	    
+	    MainDisplayClass mainDisplay = new MainDisplayClass();
+	    mainDisplay.runMenu();
+	    
+		
+	
+	}
+
+}
